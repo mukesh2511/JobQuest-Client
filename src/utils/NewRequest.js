ed40c64e4cb4e20607daee38e9e7de2bdc861269
Mukesh <mouryamukesh8692@gmail.com>
2023-09-15 19:10:00 +0530

    fixed issues

--- src/utils/NewRequest.js
+++ src/utils/NewRequest.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const NewRequest = axios.create({
-  baseURL: "http://localhost:5000/api",
-  // baseURL: "https://jobquest-api.onrender.com/api/",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://jobquest-api.onrender.com/api/",
   withCredentials: true,
 });
 
